42f788e7115a080cb994e15798ce632c3cbbf740
Aryan Vishwakarma <official.yanish.mail@gmail.com>
2025-12-12 19:40:48 +0530

    new branch

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-
-const Home = () => {
+const page = () => {
   return (
     <div>
-      Home
+        page
     </div>
   )
 }
 
-export default Home
+export default page
