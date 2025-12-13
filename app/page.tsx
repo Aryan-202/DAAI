7dbf651a3e3250455de60db8c2fce93d09028c3c
Aryan Vishwakarma <official.yanish.mail@gmail.com>
2025-12-14 01:08:44 +0530

    --

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { Input } from "@/components/layouts/Input"
+
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen ">
-      <div className="bg-black grid w-full gap-6 md:max-w-2xl lg:max-w-3xl xl:max-w-4xl m-2.5">
-        <Input />
-      </div>
+    <div className="">
+      page
     </div>
   )
 }
