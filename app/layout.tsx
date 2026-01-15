d63e2a204c20391a7eec771ee7e57c8a3fc96a81
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-01-15 19:23:02 +0530

    navigation menu added from shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+
 import { ThemeProvider } from "@/components/theme-provider";
+import  Header  from "@/components/layout/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +37,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Header />
           {children}
         </ThemeProvider>
       </body>
