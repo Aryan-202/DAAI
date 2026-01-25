11a6fc4a44491a78c76ea7b2b55a125ae44f2680
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2026-01-25 17:33:52 +0530

    feat: Implement initial Prisma ORM setup with a user model, add a sign-in form component, and integrate new image assets.

--- public/assets.ts
+++ public/assets.ts
@@ -1,7 +1,11 @@
 import googleSheetLogo from './images/google_sheets.png';
+import zohoSheetLogo from './images/zoho_sheets.png'
+import excelLogo from './svgs/excel_logo.svg'
 
 export const assets = {
     images: {
         googleSheets: googleSheetLogo,
+        zohoSheets: zohoSheetLogo,
+        excel: excelLogo,
     }
-}
+} as const;
